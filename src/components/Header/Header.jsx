422de8e512d317e2e2bc9608965d5fc5eb530264
Eugene Kovgan <jugin.kovgan87@gmail.com>
2022-02-14 16:01:00 +0300

    feat: fixed bug burger menu (not hiden)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,39 @@
 import "./Header.scss";
 import { NavLink } from "react-router-dom";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown, Link } from "react-bootstrap";
 import SocBlock from "../SocBlock/SocBlock";
 import PhonesBlock from "../PhonesBlock/PhonesBlock";
 import logo_tsv from "../../assets/img/favicon.png";
+import { useState } from "react";
 
 const Header = () => {
+    const [expanded, setExpanded] = useState(false);
+
     return (
-        <Navbar className="header" collapseOnSelect expand="lg" variant="dark" sticky="top">
-            {/* <Navbar className="header" collapseOnSelect expand="lg" variant="dark" sticky="top"></Navbar> */}
+        <Navbar expanded={expanded} className="header" collapseOnSelect expand="lg" variant="dark" sticky="top">
+            {/* <Navbar className="header" collapseOnSelect expand="lg" variant="dark" sticky="top"> */}
             {/* <h1>Заправка картриджей и ремонт принтеров </h1> */}
             <Container>
                 <NavLink className={"navbar-brand"} to="/tsv-print">
                     <img className="logo_tsv" src={logo_tsv} alt="logo_tsv" />
                     {/* TSV-print */}
                 </NavLink>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Toggle
+                    onClick={() => setExpanded(expanded ? false : "expanded")}
+                    aria-controls="responsive-navbar-nav"
+                />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <NavLink className={"nav-link"} to="/services">
+                        <NavLink onClick={() => setExpanded(false)} className={"nav-link"} to="/services">
                             Услуги
                         </NavLink>
-                        <NavLink className={"nav-link"} to="/price">
+                        <NavLink onClick={() => setExpanded(false)} className={"nav-link"} to="/price">
                             Цены
                         </NavLink>
-                        <NavLink className={"nav-link"} to="/comments">
+                        <NavLink onClick={() => setExpanded(false)} className={"nav-link"} to="/comments">
                             Отзывы
                         </NavLink>
-                        <NavLink className={"nav-link"} to="/articles">
+                        <NavLink onClick={() => setExpanded(false)} className={"nav-link"} to="/articles">
                             Статьи
                         </NavLink>
                         <NavDropdown to="/contacts" title="Контакты" id="collasible-nav-dropdown">
